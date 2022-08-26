26950dc048b358bf9d6f91bc0c3d26277789da33
Jo <loluvulol@gmail.com>
2022-08-26 11:35:52 +1000

    Finish game logic

---
+++ game.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+const lodash = require("lodash");
+
+let players = [];
+let round = 0;
+
+app.use(express.static(__dirname + "/public"));
+
+http.listen(port, function() {
+    console.log("Listening on port " + port);
+});
+
+io.on("connection", function(socket){
+    let userId;
+
+    socket.on("new player", function(id, name) {
+        userId = {
+            name: name,
+            id: id,
+            round: round,
+            roll: null,
+            winner: false
+        };
+        players.push(userId);
+        io.emit("players", players);
+    });
+
+    socket.on("disconnect", function(reason){
+        console.log(reason);
+        players = players.filter(function(obj){
+            return obj !== userId;
+        });
+        io.emit("players", players);
+    });
+
+    socket.on("roll", function(){
+        userId.roll = lodash.random(1, 1000);
+        console.log(userId);
+        nextRoundCheck();
+    });
+
+    io.emit("players", players);
+});
+
+function nextRoundCheck(){
+    if(players.length > 0){
+        let ready = 0;
+        let top = 0;
+        let win = 0;
+        players.forEach(function(player, index){
+            player.winner = false;
+            if(player.roll){
+                ready++;
+                if(player.roll && player.roll > top){
+                    win = index;
+                    top = player.roll;
+                }
+            }
+        })
+        players[win].winner = true;
+        io.emit("players", players);
+        if(ready >= players.length){
+            io.emit("inplay", "Round " + round + " winner is " + players[win].name);
+            round++;
+            players.forEach(function(player, index){
+                player.winner = false;
+                player.roll = null;
+                player.round = round;
+            })
+        }
+    }
+}
---
+++ socket.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+const http = require("http").Server(app);
+const socket = require("socket.io")(http);
+let players = [];
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function(req, res) {
+    res.sendFile("index.html")
+});
+
+socket.on("connection", (socket) => {
+    console.log("connected");
+    socket.on("new player", function(id, name) {
+        console.log(id);
+        console.log(name);
+        players.push({
+            name: name,
+            id: id
+        });
+        socket.emit("players", players);
+    });
+    socket.emit("players", players);
+})
+
+const server = http.listen(port, function() {
+    console.log("ready on " + port);
+});
